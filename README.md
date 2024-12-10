0263305d9a75158f1afbee81b23af71b403822b1
vikku <vita.201581@yandex.ru>
2024-12-10 20:15:40 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Мессенджер
+## Технологии
+1. Redis
+2. Python Tornado
+
+
+## Развертывание
+1. Клонирование репозитория
+```bash
+git clone https://github.com/vkkuznetsov/OROPO_lab9.git
+cd OROPO_lab9
+```
+
+2. Создание виртуального окружения
+```bash
+python3 -m venv venv
+```
+3. Активирование венв
+```bash
+source venv/bin/activate #linux
+или
+.\venv\Scripts\activate #windows
+```
+3. Установка зависимостей
+```bash
+pip install -r req.txt 
+```
+4. Проверка конфигурации (config/settings.yml)  
+Укажите хост и порт на котором запустится Мессенджер, и укажите хост и порт на котором работает Redis  
+По дефолту это localhost:8000 и localhost:6369  
+Если редиса нет то на windows ставится через wsl
+```bash
+sudo apt install redis-server 
+```
+На Linux эта же команда
+4. Запуск приложения
+```bash
+python main.py
+```
+docker run -d --name=prometheus -p 9090:9090 -v ${PWD}/prometheus_data/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import psutil
+from prometheus_client import Gauge, generate_latest
+from fastapi import FastAPI, Response
+from fastapi.responses import PlainTextResponse
+
+app = FastAPI()
+
+CPU_USAGE = Gauge('cpu_usage_percent', 'CPU usage percent', ['core'])
+MEMORY_TOTAL = Gauge('memory_total_bytes', 'Total memory in bytes')
+MEMORY_USED = Gauge('memory_used_bytes', 'Used memory in bytes')
+DISK_TOTAL = Gauge('disk_total_bytes', 'Total disk space in bytes', ['partition'])
+DISK_USED = Gauge('disk_used_bytes', 'Used disk space in bytes', ['partition'])
+
+
+def collect_metrics():
+
+    for i, percentage in enumerate(psutil.cpu_percent(percpu=True)):
+        CPU_USAGE.labels(core=f'core_{i}').set(percentage)
+
+    memory = psutil.virtual_memory()
+    MEMORY_TOTAL.set(memory.total)
+    MEMORY_USED.set(memory.used)
+
+    for partition in psutil.disk_partitions():
+        usage = psutil.disk_usage(partition.mountpoint)
+        DISK_TOTAL.labels(partition=partition.mountpoint).set(usage.total)
+        DISK_USED.labels(partition=partition.mountpoint).set(usage.used)
+
+
+@app.get("/", response_class=PlainTextResponse)
+def metrics():
+    collect_metrics()
+    return Response(content=generate_latest(), media_type="text/plain")
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    host = os.getenv('EXPORTER_HOST', '0.0.0.0')
+    port = int(os.getenv('EXPORTER_PORT', '8080'))
+
+    uvicorn.run(app, host=host, port=port)
